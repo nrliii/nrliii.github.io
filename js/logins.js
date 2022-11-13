352a7ac1bde5e250d8338c477ca95bae47420cad
nrliii <majmunmaksa@gmail.com>
2022-11-13 02:01:42 +0100

    one more login

--- js/logins.js
+++ js/logins.js
@@ -3,6 +3,10 @@ function check(form) {
         {
         location.href = 'destination.html';
         }
+    else if(form.userid.value == "lukexog" && form.pswrd.value == "123456")
+        {
+        location.href = 'https://www.youtube.com/c/Lukexog';
+        }
     else
         {
         alert("Error Password or Username Required");
